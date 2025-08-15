b5feceb16b72370d60c30da5974bb2c3211ea053
Thyago Monnerat <101063531+Thyago-Monnerat@users.noreply.github.com>
2025-08-15 17:28:36 +0000

    remoção da função newCard adição da função exibirCarta adição de variáveis para a segunda carta, tornando os dados de cada carta permanentes adicionado os campos pibPerCapita e densidadePopulacional

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,55 +6,122 @@
 // Siga os comentários para implementar cada parte do desafio.
 //Teste larissa
 
-//Prototipando a função newCard
-void newCard(int id);
+// Prototipando função para exibição dos dados
+void exibirCarta( 
+    int id, 
+    char codigo[4],
+    char cidade[31],
+    char estado,
+    int populacao,
+    float areaKm,
+    float pib,
+    int pontosTuristicos,
+    float pibPerCapita,
+    float densidadePopulacao
+);
 
 int main() {
+        
+    //Codigo limitado a ter 3 caracteres. Exemplo: A01, A52, A99.
+    char codigo1[4];
+    //Cidade limitada a ter 30 caracteres.
+    char cidade1[31];
+    char estado1;
+    int populacao1;
+    float areaKm1;
+    float pib1;
+    int pontosTuristicos1;
+    float pibPerCapita1;
+    float densidadePopulacao1;
+
+    char codigo2[4];
+    char cidade2[31];
+    char estado2;
+    int populacao2;
+    float areaKm2;
+    float pib2;
+    int pontosTuristicos2;
+    float pibPerCapita2;
+    float densidadePopulacao2;
+
     //Header para contextualizar a aplicação no terminal
     printf("==================== Bem-vindo(a) ao Super Trunfo Game ====================\n");
 
-    //Chamando a função que cria uma carta e passando um id para dizer se é a carta 1 ou 2
-    newCard(1);
-    newCard(2);
+
+    printf("\n---------- Cadastre a primeira carta ----------\n\n");
+
+    printf("Código da carta: ");
+    scanf("%s", codigo1);
+
+    printf("Nome da cidade: ");
+    // %[^\n] Para ler TODOS os caracteres, incluindo espaços, até encontrar \n.
+    scanf(" %[^\n]", cidade1);
+
+    printf("Letra do estado: ");
+    scanf(" %c", &estado1);
+
+    printf("População da cidade: ");
+    scanf("%d", &populacao1);
+
+    printf("Área em km² da cidade: ");
+    scanf("%f", &areaKm1);
+
+    printf("PIB da cidade: ");
+    scanf("%f", &pib1);
+
+    printf("Quantidade de pontos turísticos: ");
+    scanf("%d", &pontosTuristicos1);
+
+    //Casting de float pois população é um inteiro, o que torna o retorno de um ponto flutuante não garantido
+    pibPerCapita1 = (float) pib1 / populacao1;
+    densidadePopulacao1 = (float) populacao1 / areaKm1;
+
+    exibirCarta(1, codigo1, cidade1, estado1, populacao1, areaKm1, pib1, pontosTuristicos1, pibPerCapita1, densidadePopulacao1);
+
+    printf("\n---------- Cadastre a segunda carta ----------\n\n");
+
+    printf("Código da carta: ");
+    scanf("%s", codigo2);
+
+    printf("Nome da cidade: ");
+    scanf(" %[^\n]", cidade2);
+
+    printf("Letra do estado: ");
+    scanf(" %c", &estado2);
+
+    printf("População da cidade: ");
+    scanf("%d", &populacao2);
+
+    printf("Área em km² da cidade: ");
+    scanf("%f", &areaKm2);
+
+    printf("PIB da cidade: ");
+    scanf("%f", &pib2);
+
+    printf("Quantidade de pontos turísticos: ");
+    scanf("%d", &pontosTuristicos2);
+
+    pibPerCapita2 = (float) pib2 / populacao2;
+    densidadePopulacao2 = (float) populacao2 / areaKm2;
+
+    exibirCarta(2, codigo2, cidade2, estado2, populacao2, areaKm2, pib2, pontosTuristicos2, pibPerCapita2, densidadePopulacao2);
 
     return 0;
 }
 
-//Definindo a função para a criação de uma nova carta
-void newCard(int id){
-    //Codigo limitado a ter 3 caracteres. Exemplo: A01, A52, A99.
-    char codigo[4];
-    //Cidade limitada a ter 30 caracteres.
-    char cidade[31];
-    char estado;
-    int populacao;
-    float areaKm;
-    float pib;
-    int pontosTuristicos;
-
-    printf("\n---------- Cadastre uma carta ----------\n\n");
-
-    printf("Código da carta: ");
-    scanf("%s", codigo);
-
-    printf("Nome da cidade: ");
-    // %[^\n] Para ler TODOS os caracteres, incluindo espaços, até encontrar \n.
-    scanf(" %[^\n]", cidade);
-
-    printf("Letra do estado: ");
-    scanf(" %c", &estado);
-
-    printf("População da cidade: ");
-    scanf("%d", &populacao);
-
-    printf("Área em km² da cidade: ");
-    scanf("%f", &areaKm);
-
-    printf("PIB da cidade: ");
-    scanf("%f", &pib);
-
-    printf("Quantidade de pontos turísticos: ");
-    scanf("%d", &pontosTuristicos);
+//Definindo a função de exibição de carta
+void exibirCarta(
+    int id, 
+    char codigo[4],
+    char cidade[31],
+    char estado,
+    int populacao,
+    float areaKm,
+    float pib,
+    int pontosTuristicos,
+    float pibPerCapita,
+    float densidadePopulacao
+){
 
     printf("\n==================== Informações da carta %d: ====================\n", id);
     printf("\nCódigo: %s", codigo);
@@ -64,5 +131,7 @@ void newCard(int id){
     printf("\nÁrea: %.2fkm²", areaKm);
     printf("\nPIB: %f bilhões de reais", pib);
     printf("\nQuantidade de pontos turísticos: %d\n", pontosTuristicos);
-    printf("\n==================== ////////// ====================\n", id);
+    printf("PIB per capita: %f\n", pibPerCapita);
+    printf("Densidade populacional: %f\n", densidadePopulacao);
+    printf("\n==================== ////////// ====================\n");
 }
