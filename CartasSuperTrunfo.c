c8ff9db421593d6aac3bb8e7f88f878e978d7c83
Thyago Monnerat <101063531+Thyago-Monnerat@users.noreply.github.com>
2025-08-15 21:58:11 +0000

    adição do campo float superPoder, sendo a soma de todos os numéricos da carta. alteração do tipo de populcao e pontosTuristicos para unsigned int. criação da função compararPoder para molularizar o código. pequenas alterações em textos para melhor exibição no terminal.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -17,8 +17,12 @@ void exibirCarta(
     float pib,
     int pontosTuristicos,
     float pibPerCapita,
-    float densidadePopulacao
+    float densidadePopulacao,
+    float superPoder
 );
+
+// Prototipando função para comparar dados entre as cartas
+void compararPoder(char nomeCampo[24], float campoCarta1, float campoCarta2);
 
 int main() {
         
@@ -27,22 +31,24 @@ int main() {
     //Cidade limitada a ter 30 caracteres.
     char cidade1[31];
     char estado1;
-    int populacao1;
+    unsigned int populacao1;
     float areaKm1;
     float pib1;
-    int pontosTuristicos1;
+    unsigned int pontosTuristicos1;
     float pibPerCapita1;
     float densidadePopulacao1;
+    float superPoder1;
 
     char codigo2[4];
     char cidade2[31];
     char estado2;
-    int populacao2;
+    unsigned int populacao2;
     float areaKm2;
     float pib2;
-    int pontosTuristicos2;
+    unsigned int pontosTuristicos2;
     float pibPerCapita2;
     float densidadePopulacao2;
+    float superPoder2;
 
     //Header para contextualizar a aplicação no terminal
     printf("==================== Bem-vindo(a) ao Super Trunfo Game ====================\n");
@@ -75,8 +81,7 @@ int main() {
     //Casting de float pois população é um inteiro, o que torna o retorno de um ponto flutuante não garantido
     pibPerCapita1 = (float) pib1 / populacao1;
     densidadePopulacao1 = (float) populacao1 / areaKm1;
-
-    exibirCarta(1, codigo1, cidade1, estado1, populacao1, areaKm1, pib1, pontosTuristicos1, pibPerCapita1, densidadePopulacao1);
+    superPoder1 = (float) populacao1 + areaKm1 + pib1 + pontosTuristicos1 + pibPerCapita1 + densidadePopulacao1;
 
     printf("\n---------- Cadastre a segunda carta ----------\n\n");
 
@@ -103,8 +108,23 @@ int main() {
 
     pibPerCapita2 = (float) pib2 / populacao2;
     densidadePopulacao2 = (float) populacao2 / areaKm2;
+    superPoder2 = (float) populacao2 + areaKm2 + pib2 + pontosTuristicos2 + pibPerCapita2 + densidadePopulacao2;
 
-    exibirCarta(2, codigo2, cidade2, estado2, populacao2, areaKm2, pib2, pontosTuristicos2, pibPerCapita2, densidadePopulacao2);
+    exibirCarta(1, codigo1, cidade1, estado1, populacao1, areaKm1, pib1, pontosTuristicos1, pibPerCapita1, (1/densidadePopulacao1), superPoder1);
+    exibirCarta(2, codigo2, cidade2, estado2, populacao2, areaKm2, pib2, pontosTuristicos2, pibPerCapita2, (1/densidadePopulacao2), superPoder2);
+
+    printf("\n==================== Hora de ver quem ganhou ====================\n\n");
+
+    //Casting as variáveis int em float para simplificar a passagem de parâmetros
+    compararPoder("população", (float) populacao1, (float) populacao2);
+    compararPoder("área km²", areaKm1, areaKm2);
+    compararPoder("PIB", pib1, pib2);
+    compararPoder("pontos turísticos", pontosTuristicos1, pontosTuristicos2);
+    compararPoder("PIB per capita", pibPerCapita1, pibPerCapita2);
+    compararPoder("densidade populacional", (1/densidadePopulacao1), (1/densidadePopulacao2));
+    compararPoder("super poder", superPoder1, superPoder2);
+
+    printf("\n==================== ////////// ====================\n");
 
     return 0;
 }
@@ -120,7 +140,8 @@ void exibirCarta(
     float pib,
     int pontosTuristicos,
     float pibPerCapita,
-    float densidadePopulacao
+    float densidadePopulacao,
+    float superPoder
 ){
 
     printf("\n==================== Informações da carta %d: ====================\n", id);
@@ -133,5 +154,11 @@ void exibirCarta(
     printf("\nQuantidade de pontos turísticos: %d\n", pontosTuristicos);
     printf("PIB per capita: %f\n", pibPerCapita);
     printf("Densidade populacional: %f\n", densidadePopulacao);
-    printf("\n==================== ////////// ====================\n");
+    printf("Super poder: %f\n", superPoder);
+}
+
+//Definindo função para comparar os campos
+void compararPoder(char nomeCampo[24], float campoCarta1, float campoCarta2){
+    //Usando operador ternário para comparação e retorno
+   printf("Comparação do campo %s: %s\n",nomeCampo, campoCarta1 > campoCarta2 ? "Carta 1 venceu" : campoCarta1 < campoCarta2 ? "Carta 2 venceu" : "Empate");
 }
